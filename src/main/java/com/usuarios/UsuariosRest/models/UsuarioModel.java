6a157917211174336ffa4f9fda0810523a6fb333
Simón <31756169+Adversario@users.noreply.github.com>
2025-07-19 20:37:04 -0400

    Update UsuarioModel.java

--- src/main/java/com/usuarios/UsuariosRest/models/UsuarioModel.java
+++ src/main/java/com/usuarios/UsuariosRest/models/UsuarioModel.java
@@ -4,49 +4,18 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(name = "users")
-public class UsuarioModel {
-    public long getId() {
-        return id;
-    }
+public class User {
 
-    public void setId(long id) {
-        this.id = id;
-    }
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-
-    @Column
+    @Column(name = "first_name")
     private String firstName;
 
-    @Column
+    @Column(name = "last_name")
     private String lastName;
 
-    @Column
     private String email;
+
+    // getters & setters
 }
